766a17615802b25dae8d7ebc8ccb1f051134bafd
DW <dwlee257@gmail.com>
2026-05-05 20:36:21 +0900

    feat: 수입 도메인 추가

--- src/main/java/com/feellog/backend/global/config/SecurityConfig.java
+++ src/main/java/com/feellog/backend/global/config/SecurityConfig.java
@@ -2,7 +2,8 @@ package com.feellog.backend.global.config;
 
 import com.feellog.backend.global.jwt.JwtProvider;
 import com.feellog.backend.global.security.JwtAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,11 +16,19 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
     private final CorsConfigurationSource corsConfigurationSource;
+    
+    public SecurityConfig(
+            JwtProvider jwtProvider,
+            @Qualifier("corsConfigurationSource") CorsConfigurationSource corsConfigurationSource
+    ) {
+        this.jwtProvider = jwtProvider;
+        this.corsConfigurationSource = corsConfigurationSource;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -34,7 +43,7 @@ public class SecurityConfig {
                                 "/api/v1/auth/kakao",
                                 "/api/v1/auth/google",
                                 "/api/v1/auth/guest",
-                                "/api/v1/master-data"
+                                "/api/v1/master-data" // 이 부분만 추가
                         ).permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
