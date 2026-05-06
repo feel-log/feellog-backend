2d4849eb64da81e32c956d32aa2136dc8fb9462c
codedbyminjae <minjaekim7111@naver.com>
2026-05-07 00:52:38 +0900

    fix: 스웨거 서버 URL https로 명시

--- src/main/java/com/feellog/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/feellog/backend/global/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +20,7 @@ public class SwaggerConfig {
                 .name("bearerAuth");
 
         return new OpenAPI()
+                .addServersItem(new Server().url("https://api.feellog.xyz"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme));
     }
